29dbff6d12db3db4cf1a4236324b9e9634e2073b
Daniel C Farr <danielcfarr@gmail.com>
2018-10-25 22:07:26 -0600

    basic login setup

--- site/resources/views/includes/body-login.blade.php
+++ site/resources/views/includes/body-login.blade.php
@@ -2,17 +2,17 @@
 <html lang="{{\App::getLocale()}}" class="no-js">
 <head prefix="{{\App\Utils\SocialMarkup::makeRegisteredPrefixes()}}">
 
-	@include($includes->head)
-	@yield('head')
-	
+    @include($includes->head)
+    @yield('head')
+
 </head>
-<body class="body-login">
-	<div id="container">    
-    
-        @yield('content')
-        
-        @include($includes->footer)
+<body class="body-login bg-gray-3">
+    <div id="container">    
+	<div id="content">
+	        @yield('content')
 	</div>
+        @include($includes->footer)
+    </div>
 </body>
 </html>
 
--- site/resources/views/login/login.blade.php
+++ site/resources/views/login/login.blade.php
@@ -1,7 +1,16 @@
 @extends($includes->body)
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-lg-6">
 
-<h1>Login Form</h1>
+        </div>
+        <div class="col-12 col-lg-6">
+            <h1>Login Form</h1>
+        </div>
+    </div>
+</div>
+
 
 @endsection
